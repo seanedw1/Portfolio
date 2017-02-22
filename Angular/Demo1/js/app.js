602a3095d2443aed05e1b3bab78f6e3b838e8d8b
seanedw1 <sedwards@fullsail.edu>
2017-02-21 20:27:37 -0500

    added first angular classwork files

---
+++ Angular/Demo1/js/app.js
@@ -0,0 +1,53 @@
+var app = angular.module('employeeApp', []);
+
+app.controller('employeeController', function($scope, employeeService){
+    $scope.employee = {};
+    $scope.dataArr = employeeService.getEmployee();
+    
+    $scope.addEmployee = function(){
+        employeeService.addEmployee($scope.employee);
+        $scope.employee ={};
+    }
+    
+    $scope.removeEmployee = function(pIndex){
+       employeeService.removeEmployee(pIndex); 
+    }
+    
+});
+
+app.service('employeeService', function(){
+    dataArr = [];
+    
+    this.getEmployee = function(){
+        var str = localStorage.getItem("candyLs");
+        dataArr = JSON.parse(str) || dataArr;
+        return dataArr;
+    }
+    
+    this.addEmployee = function(pData){
+        
+        dataArr.push(pData);
+        var str = JSON.stringify(dataArr);
+        localStorage.setItem("candyLs",str);
+    };
+    
+    this.removeEmployee = function(pIndex){
+        dataArr.splice(pIndex, 1);
+        var str = JSON.stringify(dataArr);
+        localStorage.setItem("candyLs",str);
+    }
+    
+    
+});
+
+
+
+var delSel = function(){
+
+if(document.getElementByClass(dataArr) = checked = true){
+console.log('working')
+}else{
+console.log('not working')
+};
+
+};
