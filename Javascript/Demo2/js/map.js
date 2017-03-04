f7910e2ee4765f815c9fed2a63936b0aaa329e18
seanedw1 <sedwards@fullsail.edu>
2017-03-04 17:57:08 -0500

    added more comments cleaned up file

--- Javascript/Demo2/js/map.js
+++ Javascript/Demo2/js/map.js
@@ -1,4 +1,6 @@
 var map;
+
+// initialize map
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 43.731476, lng: -79.762545},
@@ -6,22 +8,23 @@ function initMap() {
         disableDefaultUI: false
   });
 
+// add traffic layer to map
 var trafficLayer = new google.maps.TrafficLayer();
   trafficLayer.setMap(map);
 
-//set marker
+// set marker
 marker = new google.maps.Marker({
     map: map,
     draggable: true,
-    // style of animation
+    // style of marker animation
     animation: google.maps.Animation.DROP,
     position: {lat: 43.731476, lng: -79.762545}
   });
-  // adds bounce on click
+  // adds bounce to marker on click
   marker.addListener('click', toggleBounce);
 }
 
-// bounce toggle
+// bounce toggle function
 function toggleBounce() {
   if (marker.getAnimation() !== null) {
     marker.setAnimation(null);
