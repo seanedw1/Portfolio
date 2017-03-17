75f6fb7f450f5d39d066af15b6dca01cf5d002dd
seanedw1 <sedwards@fullsail.edu>
2017-03-17 01:58:23 -0400

    cleaning up js files

--- Javascript/Demo2/js/map.js
+++ Javascript/Demo2/js/map.js
@@ -2,34 +2,40 @@ var map;
 
 // initialize map
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 43.731476, lng: -79.762545},
-    zoom: 8,
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 43.731476,
+            lng: -79.762545
+        },
+        zoom: 8,
         disableDefaultUI: false
-  });
+    });
 
-// add traffic layer to map
-var trafficLayer = new google.maps.TrafficLayer();
-  trafficLayer.setMap(map);
+    // add traffic layer to map
+    var trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map);
 
-// set marker
-marker = new google.maps.Marker({
-    map: map,
-    draggable: true,
-    // style of marker animation
-    animation: google.maps.Animation.DROP,
-    position: {lat: 43.731476, lng: -79.762545}
-  });
-  // adds bounce to marker on click
-  marker.addListener('click', toggleBounce);
+    // set marker
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        // style of marker animation
+        animation: google.maps.Animation.DROP,
+        position: {
+            lat: 43.731476,
+            lng: -79.762545
+        }
+    });
+    // adds bounce to marker on click
+    marker.addListener('click', toggleBounce);
 }
 
 // bounce toggle function
 function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-  };
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    };
 
 }
