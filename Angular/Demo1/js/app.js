54adc4fde9d19ebc10963890e6769f64caec9c50
seanedw1 <sedwards@fullsail.edu>
2017-03-16 22:17:12 -0400

    cleaned up files

--- Angular/Demo1/js/app.js
+++ Angular/Demo1/js/app.js
@@ -1,53 +1,47 @@
 var app = angular.module('employeeApp', []);
 
-app.controller('employeeController', function($scope, employeeService){
+app.controller('employeeController', function($scope, employeeService) {
     $scope.employee = {};
     $scope.dataArr = employeeService.getEmployee();
-    
-    $scope.addEmployee = function(){
+
+    $scope.addEmployee = function() {
         employeeService.addEmployee($scope.employee);
-        $scope.employee ={};
+        $scope.employee = {};
     }
-    
-    $scope.removeEmployee = function(pIndex){
-       employeeService.removeEmployee(pIndex); 
+
+    $scope.removeEmployee = function(pIndex) {
+        employeeService.removeEmployee(pIndex);
     }
-    
+
 });
 
-app.service('employeeService', function(){
+app.service('employeeService', function() {
     dataArr = [];
-    
-    this.getEmployee = function(){
+
+    this.getEmployee = function() {
         var str = localStorage.getItem("candyLs");
         dataArr = JSON.parse(str) || dataArr;
         return dataArr;
     }
-    
-    this.addEmployee = function(pData){
-        
+
+    this.addEmployee = function(pData) {
+
         dataArr.push(pData);
         var str = JSON.stringify(dataArr);
-        localStorage.setItem("candyLs",str);
+        localStorage.setItem("candyLs", str);
     };
-    
-    this.removeEmployee = function(pIndex){
+
+    this.removeEmployee = function(pIndex) {
         dataArr.splice(pIndex, 1);
         var str = JSON.stringify(dataArr);
-        localStorage.setItem("candyLs",str);
+        localStorage.setItem("candyLs", str);
     }
-    
-    
 });
 
-
-
-var delSel = function(){
-
-if(document.getElementByClass(dataArr) = checked = true){
-console.log('working')
-}else{
-console.log('not working')
-};
-
+var delSel = function() {
+    if (document.getElementByClass(dataArr) = checked = true) {
+        console.log('working')
+    } else {
+        console.log('not working')
+    };
 };
