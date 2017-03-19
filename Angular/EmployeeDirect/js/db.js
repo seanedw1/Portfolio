b91940ca22c8bd57e31154538b65b491b302f731
seanedw1 <sedwards@fullsail.edu>
2017-03-19 19:12:21 -0400

    renamed angular folder for symantics

---
+++ Angular/EmployeeDirect/js/db.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyCC4RLDLerNDBPdKa9iKFJfFdLANioD_N4",
+   authDomain: "employee-6d3af.firebaseapp.com",
+   databaseURL: "https://employee-6d3af.firebaseio.com",
+   storageBucket: "employee-6d3af.appspot.com",
+   messagingSenderId: "829186031442"
+ };
+ firebase.initializeApp(config);
