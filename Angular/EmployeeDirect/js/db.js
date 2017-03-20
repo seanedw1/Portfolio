aa11380e9b458f72fc4ff9da17c9fb614e9c8250
seanedw1 <sedwards@fullsail.edu>
2017-03-19 20:10:04 -0400

    delete

--- Angular/EmployeeDirect/js/db.js
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
- var config = {
-   apiKey: "AIzaSyCC4RLDLerNDBPdKa9iKFJfFdLANioD_N4",
-   authDomain: "employee-6d3af.firebaseapp.com",
-   databaseURL: "https://employee-6d3af.firebaseio.com",
-   storageBucket: "employee-6d3af.appspot.com",
-   messagingSenderId: "829186031442"
- };
- firebase.initializeApp(config);
